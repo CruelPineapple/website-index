55efd6c3717c92514c60631e3a6ab8a398eb36e4
OMEN2 <1617218599@qq.com>
2021-04-10 00:44:16 +0800

    add icp

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="icp">
+      互联网ICP备案：
+      <a href="https://beian.miit.gov.cn/" target="_blank">蜀ICP备2021008207号-1</a>
+    </div>
   </div>
 </template>
 
@@ -24,5 +28,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.icp{
+  width: 100%;
+  position: fixed;
+  bottom: 0;
 }
 </style>
