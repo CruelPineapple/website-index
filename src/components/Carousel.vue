44b65cf5c1ff8eb5d5d4ffa4f360162ab0656276
OMEN2 <1617218599@qq.com>
2021-04-11 23:45:01 +0800

    finish carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,16 +1,63 @@
 <template>
-    
+  <el-carousel :interval="4000" type="card" height="300px">
+    <el-carousel-item class="item1">
+      <h2 @click="Log">Log</h2>
+      <h4 @click="Log">讲故事</h4>
+      <img @click="Log" class="arrow" src="../assets/编辑.png" />
+    </el-carousel-item>
+    <el-carousel-item class="item2">
+      <h2 @click="BootCamp">BootCamp</h2>
+      <h4 @click="BootCamp">练习场</h4>
+      <img @click="BootCamp" class="arrow" src="../assets/项目.png" />
+    </el-carousel-item>
+    <el-carousel-item class="item3">
+      <h2 @click="Experience">Experience</h2>
+      <h4 @click="Experience">笔记本</h4>
+      <img @click="Experience" class="arrow" src="../assets/公开 结果.png" />
+    </el-carousel-item>
+  </el-carousel>
 </template>
 
 <script>
-
 export default {
-    name: 'Carousel',
-}
-
-
+  name: "Carousel",
+  methods: {
+    Log: function () {
+      this.$notify({
+        title: "提示",
+        message: "页面开发中",
+        type: "warning",
+      });
+    },
+    BootCamp: function () {
+      this.$notify({
+        title: "提示",
+        message: "页面开发中",
+        type: "warning",
+      });
+    },
+    Experience: function () {
+      this.$notify({
+        title: "提示",
+        message: "页面开发中",
+        type: "warning",
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+.item1 {
+  background-color: #e6e6e6bb;
+}
+.item2 {
+  background-color: #e6e6e6bb;
+}
+.item3 {
+  background-color: #e6e6e6bb;
+}
+.arrow {
+  width: 100px;
+}
 </style>
