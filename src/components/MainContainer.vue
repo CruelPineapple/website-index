8054986eac908994340a94fdad04d230be54d973
OMEN2 <1617218599@qq.com>
2021-04-11 22:23:59 +0800

    add main container component

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="MainContainer">
+    <img class="logo" alt="Rice Shower!!!" src="../assets/newrice.png" />
+    <div class="carousel-container">
+      <Carousel />
+    </div>
+  </div>
+</template>
+
+<script>
+import Carousel from "../components/Carousel.vue";
+
+export default {
+  name: "MainContainer",
+  components: {
+    Carousel,
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  height: 300px;
+  scale: 1;
+}
+.carousel-container {
+  width: 900px;
+  float: right;
+}
+</style>
